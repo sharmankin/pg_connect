917de02f1846dd9a711b425af75256723fae7caa
karpo <me@karpovan.ru>
2023-07-31 20:00:21 +0300

    0.0.4

--- pg3_connect/pg3/__init__.py
+++ pg3_connect/pg3/__init__.py
@@ -1,7 +1,14 @@
+import sys
+
 import dotenv as _de
 from .connections import *
 from .extras import *
+import os
 
 _de.load_dotenv(
     _de.find_dotenv('project.env', raise_error_if_not_found=True)
 )
+
+if not os.getenv('PG_NODE'):
+    print('Please add `PG_NODE` into your project.env')
+    sys.exit(1)
